e3193550a99b84436e1cf2e2d303ec8aefbcdf59
Istinj <istinj@gmail.com>
2015-11-06 13:48:24 +0100

    Catmull subdivision implementing
    Funzione di subdiv

--- tesselation.cpp
+++ tesselation.cpp
@@ -58,7 +58,8 @@ void smooth_normals(Mesh* mesh) {
 	for (auto triangle_ : mesh->triangle)
 	{
 		// compute face normal
-		auto cross_prod = cross((mesh->pos[triangle_.y] - mesh->pos[triangle_.x]), (mesh->pos[triangle_.z] - mesh->pos[triangle_.x]));
+		auto cross_prod = cross((mesh->pos[triangle_.y] - mesh->pos[triangle_.x]), 
+			(mesh->pos[triangle_.z] - mesh->pos[triangle_.x]));
 		auto temp_norm = normalize(cross_prod);
 		// accumulate face normal to the vertex normals of each face index
 		norm[triangle_.x] += temp_norm;
@@ -69,8 +70,10 @@ void smooth_normals(Mesh* mesh) {
 	for (auto quad_ : mesh->quad)
 	{
 		// compute face normal
-		auto c1 = cross((mesh->pos[quad_.y] - mesh->pos[quad_.x]), (mesh->pos[quad_.z] - mesh->pos[quad_.x]));
-		auto c2 = cross((mesh->pos[quad_.y] - mesh->pos[quad_.w]), (mesh->pos[quad_.z] - mesh->pos[quad_.w]));
+		auto c1 = cross((mesh->pos[quad_.y] - mesh->pos[quad_.x]),
+			(mesh->pos[quad_.z] - mesh->pos[quad_.x]));
+		auto c2 = cross((mesh->pos[quad_.y] - mesh->pos[quad_.w]), 
+			(mesh->pos[quad_.z] - mesh->pos[quad_.w]));
 
 		auto temp_quad_norm1 = normalize(c1);
 		auto temp_quad_norm2 = normalize(c2);
@@ -108,20 +111,20 @@ void subdivide_catmullclark(Mesh* subdiv) {
     // skip is needed
     // allocate a working Mesh copied from the subdiv
 	Mesh* temp_mesh = subdiv;
-	
     // foreach level
-	for (int i = 0; i < subdiv->subdivision_catmullclark_level; i++)
+	for (int i = 0; i < temp_mesh->subdivision_catmullclark_level; i++)
 	{
 		// make empty pos and quad arrays
-		auto pos_array = std::vector<vec3f>(subdiv->pos.size(), zero3f);
-		auto quad_array = std::vector<vec4i>(subdiv->pos.size(), zero4i);
+		auto pos_array = std::vector<vec3f>();
+		auto quad_array = std::vector<vec4i>(temp_mesh->triangle.size() * 3 + temp_mesh->quad.size() * 4, zero4i);
 
 		// create edge_map from current mesh
 		EdgeMap edgeM_ = EdgeMap(temp_mesh->triangle, temp_mesh->quad);
 		
 		// linear subdivision - create vertices
 		// copy all vertices from the current mesh
-		auto starting_verteces = std::vector<vec3f>(temp_mesh->pos);
+		pos_array = temp_mesh->pos;
+
 		// add vertices in the middle of each edge (use EdgeMap)
 		vec3f midpoint;
 		auto e_list = edgeM_.edges();
@@ -131,46 +134,132 @@ void subdivide_catmullclark(Mesh* subdiv) {
 			int startIndex = ed_.x;
 			int finalInedx = ed_.y;
 			midpoint = (temp_mesh->pos[startIndex] + temp_mesh->pos[finalInedx])/2;
-			midpoint_array[edgeM_.edge_index(ed_)] = midpoint; // 
+			midpoint_array[edgeM_.edge_index(ed_)] = midpoint; 
+			//edgeM_._add_edge(startIndex, finalInedx);
 		}
+
 		// add vertices in the middle of each triangle
 		vec3f tri_centroid;
 		auto tri_centroid_array = std::vector<vec3f>(temp_mesh->triangle.size(), zero3f);
 		int k = 0;
 		for (auto triangle_ : temp_mesh->triangle)
 		{
-			tri_centroid = (temp_mesh->pos[triangle_.x] + temp_mesh->pos[triangle_.y] + temp_mesh->pos[triangle_.z])/3;
+			tri_centroid = (temp_mesh->pos[triangle_.x] + 
+				temp_mesh->pos[triangle_.y] + 
+				temp_mesh->pos[triangle_.z])/3;
 			tri_centroid_array[k] = tri_centroid;
 			k++;
 		}
+
 		// add vertices in the middle of each quad
 		vec3f quad_centroid;
 		auto quad_centroid_array = std::vector<vec3f>(temp_mesh->quad.size(), zero3f);
 		k = 0;
 		for (auto quad_ : temp_mesh->quad)
 		{
-			quad_centroid = (temp_mesh->pos[quad_.x] + temp_mesh->pos[quad_.y] + temp_mesh->pos[quad_.z] + temp_mesh->pos[quad_.w])/4;
+			quad_centroid = (temp_mesh->pos[quad_.x] + 
+				temp_mesh->pos[quad_.y] + 
+				temp_mesh->pos[quad_.z] + 
+				temp_mesh->pos[quad_.w])/4;
 			quad_centroid_array[k] = quad_centroid;
 			k++;
 		}
+
+		// Append all the vertices in pos-array vector
+		pos_array.insert(pos_array.end(), midpoint_array.begin(), midpoint_array.end());
+		pos_array.insert(pos_array.end(), tri_centroid_array.begin(), tri_centroid_array.end());
+		pos_array.insert(pos_array.end(), quad_centroid_array.begin(), quad_centroid_array.end());
+
 		// subdivision pass --------------------------------
 		// compute an offset for the edge vertices
+		auto edge_offset = temp_mesh->pos.size();
 		// compute an offset for the triangle vertices
+		auto tri_offset = edge_offset + midpoint_array.size();
 		// compute an offset for the quad vertices
+		auto quad_offset = tri_offset + tri_centroid_array.size();
+
 		// foreach triangle
-		// add three quads to the new quad array
+		for (int i = 0; i < temp_mesh->triangle.size(); i+=3)
+		{
+			quad_array[i] = vec4i(	i,
+									edge_offset + i, 
+									tri_offset + i, 
+									edge_offset + 1 + i);
+
+			quad_array[i + 1] = vec4i(	(i + 1), 
+										edge_offset + 1 + (i + 1), 
+										tri_offset + i, 
+										edge_offset + i);
+
+			quad_array[i + 2] = vec4i(	(i + 2), 
+										edge_offset + (i + 1), 
+										tri_offset + i, 
+										edge_offset + 1 + (i + 1));
+		}
+		
 		// foreach quad
-		// add four quads to the new quad array
+		//midpoint_offset = edge_offset e qua
+		for ( ; i < temp_mesh->triangle.size() * 3 + temp_mesh->quad.size(); i += 4)
+		{
+			// add four quads to the new quad array
+			quad_array[i] = vec4i(	i,
+									edge_offset + i,
+									quad_offset + i,
+									edge_offset + i + 3);
+
+			quad_array[i] = vec4i(	i + 1,
+									edge_offset + i + 1,
+									quad_offset + i,
+									edge_offset + i);
+
+			quad_array[i] = vec4i(	i + 2,
+									edge_offset + i + 2,
+									quad_offset + i,
+									edge_offset + i + 1);
+
+			quad_array[i] = vec4i(	i + 3,
+									edge_offset + i + 3,
+									quad_offset + i,
+									edge_offset + i + 2);
+		}
+
 		// averaging pass ----------------------------------
 		// create arrays to compute pos averages (avg_pos, avg_count)
 		// arrays have the same length as the new pos array, and are init to zero
+		auto avg_pos = std::vector<vec3f>(pos_array.size(), zero3f);
+		auto avg_count = std::vector<int>(pos_array.size(), 0);
 		// for each new quad
-		// compute quad center using the new pos array
-		// foreach vertex index in the quad
+		for (auto quad_ : quad_array)
+		{
+			// compute quad center using the new pos array
+			auto c = (	pos_array[quad_.x] +
+						pos_array[quad_.y] +
+						pos_array[quad_.z] +
+						pos_array[quad_.w]) / 4;
+
+			// foreach vertex index in the quad
+			for (int j = 0; j < 4; j++)
+			{
+				int v_index = quad_[j];
+				avg_pos[v_index] += c;
+				avg_count[v_index]++;
+			}
+		}
+
 		// normalize avg_pos with its count avg_count
+		for (int i = 0; i < avg_pos.size(); i++)
+			avg_pos[i] = avg_pos[i] / avg_count[i];
+
 		// correction pass ----------------------------------
 		// foreach pos, compute correction p = p + (avg_p - p) * (4/avg_count)
+		for (int v_index = 0; v_index < avg_pos.size(); v_index++)
+			pos_array[v_index] += (avg_pos[v_index] - pos_array[v_index]) * (4 / avg_count[v_index]);
+
 		// set new arrays pos, quad back into the working mesh; clear triangle array
+		temp_mesh->pos = pos_array;
+		temp_mesh->quad = quad_array;
+		temp_mesh->triangle.clear();
+		//delete[] &temp_mesh->triangle;
 	}
     // clear subdivision
     // according to smooth, either smooth_normals or facet_normals
@@ -183,17 +272,22 @@ void subdivide_bezier(Mesh* bezier) {
     // YOUR CODE GOES HERE ---------------------
     // skip is needed
     // allocate a working polyline from bezier
+	Mesh* w_poly = new Mesh(*bezier);
     // foreach level
-        // make new arrays of positions and bezier segments
-        // copy all the vertices into the new array (this waste space but it is easier for now)
-        // foreach bezier segment
-            // apply subdivision algorithm
-            // prepare indices for two new segments
-            // add mid point
-            // add points for first segment and fix segment indices
-            // add points for second segment and fix segment indices
-            // add indices for both segments into new segments array
-        // set new arrays pos, segments into the working lineset
+	for (int i = 0; i < w_poly->subdivision_bezier_level; i++)
+	{
+		// make new arrays of positions and bezier segments
+		// copy all the vertices into the new array (this waste space but it is easier for now)
+		// foreach bezier segment
+		// apply subdivision algorithm
+		// prepare indices for two new segments
+		// add mid point
+		// add points for first segment and fix segment indices
+		// add points for second segment and fix segment indices
+		// add indices for both segments into new segments array
+		// set new arrays pos, segments into the working lineset
+	}
+
     // copy bezier segments into line segments
     // clear bezier array from lines
     // run smoothing to get proper tangents
